43a872f37a3d6fe4407af3f0de8734f41e6015b8
Thomas Regourd <tom.reg@hotmail.fr>
2022-10-11 11:05:13 +0200

    terms .md ok

--- src/pages/terms.js
+++ src/pages/terms.js
@@ -3,12 +3,24 @@ import { PageWrapper } from "~components/Core"
 import TermsConditionSection from "~sections/utility/TermsCondition"
 import Footer from "~sections/shared/Footer"
 import { NecohHeader } from "../libs/NecohHeader"
+import { graphql } from "gatsby"
 
-export default function TermsAndConditions() {
+export default function TermsAndConditions({ data }) {
   return (
     <PageWrapper headerConfig={NecohHeader}>
-      <TermsConditionSection />
+      <TermsConditionSection content={data.terms?.frontmatter} />
       <Footer />
     </PageWrapper>
   )
 }
+
+export const query = graphql`
+  query {
+    terms: markdownRemark(fields: { slug: { eq: "/terms" } }) {
+      frontmatter {
+        title
+        text
+      }
+    }
+  }
+`
--- src/sections/utility/TermsCondition/TermsConditionSection.js
+++ src/sections/utility/TermsCondition/TermsConditionSection.js
@@ -1,41 +1,22 @@
+import Markdown from "markdown-to-jsx"
 import React from "react"
 import { Col, Container, Row } from "react-bootstrap"
 import Terms from "./style"
-export default function TermsCondition() {
+export default function TermsCondition({ content }) {
   return (
     <Terms>
       <Container>
         <Row className="justify-content-center">
           <Col className="col-xl-7 col-lg-8 col-md-11 text-center">
             <Terms.Box>
-              <Terms.Title as="h2">Mentions Légales </Terms.Title>
+              <Terms.Title as="h2">{content.title}</Terms.Title>
             </Terms.Box>
           </Col>
         </Row>
         <Row className="justify-content-center">
           <Col className="col-xxl-8 col-xl-9 col-lg-10">
             <Terms.Content>
-              <Terms.Text>
-                Merci de lire avec attention les différentes modalités
-                d’utilisation du présent site avant d’y parcourir ses pages. En
-                vous connectant sur ce site, vous acceptez sans réserves les
-                présentes modalités.
-              </Terms.Text>
-              <Terms.ContentBox>
-                <Terms.TitleSmall as="h5">
-                  Propriété intellectuelle
-                </Terms.TitleSmall>
-                <Terms.Text>
-                  Le présent site Internet ainsi que le nom de domaine sont la
-                  propriété de Necoh. Le contenu du site necoh.fr tel que
-                  données, informations, marques, illustrations, logos, vidéos,
-                  animations 3D est protégé au titre du droit d’auteur et autres
-                  droits de propriété intellectuelle. Toute copie, traduction,
-                  adaptation, diffusion, représentation ou reproduction
-                  intégrale ou partielle du contenu du site est interdite et
-                  donnerait lieu à des poursuites pénales.
-                </Terms.Text>
-              </Terms.ContentBox>
+              <Markdown>{content.text}</Markdown>
             </Terms.Content>
           </Col>
         </Row>
