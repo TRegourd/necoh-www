293c1101dff96b318fb872d04779a1dce1bc8497
Thomas Regourd <tom.reg@hotmail.fr>
2022-10-12 10:36:02 +0200

    color change on breadcrumb services

--- src/sections/shared/BreadCrumb/BreadCrumb.js
+++ src/sections/shared/BreadCrumb/BreadCrumb.js
@@ -3,6 +3,8 @@ import BreadCrumb from "./style"
 import { icons } from "../../../libs/icons"
 import { Col, Row, Container } from "react-bootstrap"
 import { SuperTag } from "~components"
+import SVG from "react-inlinesvg"
+
 export default function BreadCrumbSection({ colClasses, content, ...rest }) {
   return (
     <BreadCrumb backgroundColor="#f3f4f6" {...rest}>
@@ -17,7 +19,20 @@ export default function BreadCrumbSection({ colClasses, content, ...rest }) {
         </Row>
       </Container>
       <BreadCrumb.Image>
-        <img src={icons["hero-pie-left.svg"]} alt="BreadCrumb Shape" />
+        {content.color ? (
+          <SVG
+            src={icons["hero-pie-left.svg"]}
+            preProcessor={code =>
+              content.color !== undefined &&
+              code.replace(
+                /fill: [A-Za-z0-9]+\([^)]*\)/g,
+                `fill: ${content.color}`
+              )
+            }
+          />
+        ) : (
+          <SVG src={icons["hero-pie-left.svg"]} />
+        )}
       </BreadCrumb.Image>
     </BreadCrumb>
   )
