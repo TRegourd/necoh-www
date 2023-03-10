3f628eb575d2f62445f144fdbbe1f542718be9a0
Thomas Regourd <tom.reg@hotmail.fr>
2023-03-10 08:53:08 +0100

    hidden pages in production test

--- src/components/Core/Header/menuItems.js
+++ src/components/Core/Header/menuItems.js
@@ -1,4 +1,4 @@
-export const menuItems = [
+const menus = [
   {
     name: "",
     label: "Accueil",
@@ -45,3 +45,18 @@ export const menuItems = [
     ],
   },
 ]
+
+function filterMenuItems(menus) {
+  if (process.env.NODE_ENV === "production") {
+    const namesToFilter = ["services", "emploi", "clients"]
+
+    return menus.filter(item => {
+      if (item.items) {
+        item.items = filterMenuItems(item.items, namesToFilter)
+      }
+      return !namesToFilter.includes(item.name)
+    })
+  } else return menus
+}
+
+export const menuItems = filterMenuItems(menus)
